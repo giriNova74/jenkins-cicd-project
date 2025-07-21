5331dde33afcf7aefa6a20e9b22872621518bbd9
Giri Prasad <giriprasad777sdv@gmail.com>
2025-07-22 00:25:01 +0100

    fix: deprecated dynamodb_table paramater

--- Backend.tf
+++ Backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "new-cici-bucket"   
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
-    dynamodb_table = "dynamodb-db-table-1"
+    region         = "us-east-1"
+    use_lockfile   = true
+    #dynamodb_table = "dynamodb-db-table-1"
   }
 }
